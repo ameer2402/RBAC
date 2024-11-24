60047db2e145a430dac304db6039061bc4960502
MOHAMMED AMEERKHAN <130216857+ameer2402@users.noreply.github.com>
2024-11-24 13:04:05 +0530

    Update app.js

--- role-based-access-control/app.js
+++ role-based-access-control/app.js
@@ -80,7 +80,6 @@ const PORT = process.env.PORT || 5000;
 // Making a connection to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
-    dbName: process.env.DB_NAME,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
